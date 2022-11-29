903ac5d67fe137e3a25a38fe89006ca141859087
Sven Anders Robbestad <1560743+robbestad@users.noreply.github.com>
2022-11-29 12:27:21 +0100

    added readme and renamed to seed

--- src/main.rs
+++ src/main.rs
@@ -5,26 +5,27 @@ fn main() {
     //           diamonds    clubs       hearts      spades
     let col = ["\u{2666}", "\u{2663}", "\u{2665}", "\u{2660}"];
     let val = [
-        "K", "Q", "J", "T", "9", "8", "7", "6", "5", "4", "3", "2", "A", "K", "Q", "J", "T", "9",
-        "8", "7", "6", "5", "4", "3", "2", "A", "K", "Q", "J", "T", "9", "8", "7", "6", "5", "4",
-        "3", "2", "A", "K", "Q", "J", "T", "9", "8", "7", "6", "5", "4", "3", "2", "A", "K", "Q",
-        "J", "T", "9", "8", "7", "6", "5", "4", "3", "2", "A", "K", "Q", "J", "T", "9", "8", "7",
-        "6", "5", "4", "3", "2", "A", "K", "Q", "J", "T", "9", "8", "7", "6", "5", "4", "3", "2",
-        "A", "K", "Q", "J", "T", "9", "8", "7", "6", "5", "4", "3", "2", "A",
+        "K", "Q", "J", "T", "9", "8", "7", "6", "5", "4", "3", "2", "A", 
+        "K", "Q", "J", "T", "9", "8", "7", "6", "5", "4", "3", "2", "A", 
+        "K", "Q", "J", "T", "9", "8", "7", "6", "5", "4", "3", "2", "A", 
+        "K", "Q", "J", "T", "9", "8", "7", "6", "5", "4", "3", "2", "A", 
+        "K", "Q", "J", "T", "9", "8", "7", "6", "5", "4", "3", "2", "A", 
+        "K", "Q", "J", "T", "9", "8", "7", "6", "5", "4", "3", "2", "A", 
+        "K", "Q", "J", "T", "9", "8", "7", "6", "5", "4", "3", "2", "A", 
+        "K", "Q", "J", "T", "9", "8", "7", "6", "5", "4", "3", "2", "A", 
     ];
 
-    let mut doc: Vec<String> = col
+    let mut cards: Vec<String> = col
         .iter()
         .flat_map(|c| val.iter().clone().map(move |v| v.to_string() + c))
         .collect();
 
-    let data: String = std::fs::read_to_string("random.txt")
+    let text: String = std::fs::read_to_string("seed.txt")
         .unwrap()
         .to_owned()
         .replace("\n", "");
-    let seed: [u8; 32] = data.as_bytes().try_into().unwrap();
-    println!("seed {}", data);
+    let seed: [u8; 32] = text.as_bytes().try_into().unwrap();
     let mut seeded_rng = StdRng::from_seed(seed);
-    doc.shuffle(&mut seeded_rng);
-    println!("{} deck shuffle:\n{:?}\n", doc.len() / 52, doc);
+    cards.shuffle(&mut seeded_rng);
+    println!("{} deck shuffle:\n{:?}\n", cards.len() / 52, cards);
 }
